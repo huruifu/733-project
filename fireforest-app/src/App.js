abd671416c3f367ee4da1abebbe2822d8426e73e
huruifu <rha83@sfu.ca>
2022-03-22 17:16:01 -0700

    set up web architecture

---
+++ fireforest-app/src/App.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div>
+      <Button variant="primary">Primary</Button>{' '}
+    </div>
+  );
+}
+
+export default App;
