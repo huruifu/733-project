c5b65c7ef20077c8309273f2def6e4bd988a160e
huruifu <rha83@sfu.ca>
2022-04-04 18:38:53 -0700

    update with weather eda

--- weather.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-frames = []
-for i in range(9):
-  temp = pd.read_csv(f"weather-merge/merged_{i+1}.csv", sep=",")
-  frames.append(temp)
-weather = pd.concat(frames)
-
-weather.to_csv("weather.csv")
